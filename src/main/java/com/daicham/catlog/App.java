ff5d8a75ab1520a39d9c95549fba4e5af95dda26
daicham <daicham@gmail.com>
2017-11-19 14:18:55 +0900

    Add images

--- src/main/java/com/daicham/catlog/App.java
+++ src/main/java/com/daicham/catlog/App.java
@@ -2,8 +2,10 @@ package com.daicham.catlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class App {
 
 	public static void main(String[] args) {
